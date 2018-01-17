8ed35dca7ef7c41eb17bf9bd42857310c688c553
Karen Kong <kkong006@ucr.edu>
2018-01-17 00:23:45 -0800

    Add pagination

--- app/src/main/java/appricottsoftware/clarity/MainActivity.java
+++ app/src/main/java/appricottsoftware/clarity/MainActivity.java
@@ -2,6 +2,8 @@ package appricottsoftware.clarity;
 
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,21 +18,49 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import appricottsoftware.clarity.adapters.EndlessRecyclerViewScrollListener;
+import appricottsoftware.clarity.adapters.SearchResultsListAdapter;
 import appricottsoftware.clarity.models.Podcast;
 import appricottsoftware.clarity.sync.ClarityApp;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
 import static appricottsoftware.clarity.sync.ClarityApp.getGson;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "MainActivity";
+
     private ArrayList<Podcast> searchResults;
+    private SearchResultsListAdapter searchResultsAdapter;
+    private EndlessRecyclerViewScrollListener scrollListener;
+
+    private String query;
+    private int offset;
+
+    @BindView(R.id.rvSearchResults) RecyclerView rvSearchResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        searchResults = new ArrayList<>();
+        searchResultsAdapter = new SearchResultsListAdapter(searchResults);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvSearchResults.setLayoutManager(linearLayoutManager);
+        rvSearchResults.setAdapter(searchResultsAdapter);
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                getPodcasts(query);
+            }
+        };
+        rvSearchResults.addOnScrollListener(scrollListener);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,11 +68,14 @@ public class MainActivity extends AppCompatActivity {
         final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
+            public boolean onQueryTextSubmit(String q) {
+                searchResults.clear();
+                searchResultsAdapter.notifyDataSetChanged();
+                scrollListener.resetState();
                 // Focus on the activity, minimize keyboard
                 searchView.clearFocus();
                 // Query for podcasts once user enters text
-                getPodcasts(query);
+                getPodcasts(q);
                 return true;
             }
 
@@ -56,10 +89,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Make an API call to get podcasts
-    private void getPodcasts(String query) {
+    private void getPodcasts(String q) {
+        if(this.query != q) {
+            this.query = q;
+        }
         Log.e("MainActivity", "On Query text" + query);
         // Specify the callback functions for the response handler
-        ClarityApp.getRestClient().getFullTextSearch(0, query, 0, "episode", new JsonHttpResponseHandler() {
+        ClarityApp.getRestClient().getFullTextSearch(offset, query, 0, "episode", new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
@@ -105,10 +141,13 @@ public class MainActivity extends AppCompatActivity {
     private void addPodcasts(JSONObject response) {
         // Convert the JSON into Podcasts objects (our model)
         try {
+            offset = response.getInt("next_offset");
             JSONArray resp = response.getJSONArray("results");
-            for(int i = 0; i < response.length(); i++) {
+            Log.e(TAG, resp.toString());
+            for(int i = 0; i < resp.length(); i++) {
                 Podcast p = getGson().fromJson(String.valueOf(resp.getJSONObject(i)), Podcast.class);
                 searchResults.add(p);
+                searchResultsAdapter.notifyItemInserted(searchResults.size() - 1);
                 Log.e("MainActivityAddPodcasts", String.valueOf(resp.getJSONObject(i)));
             }
         } catch(JSONException e) {
