4083f8f55afdecc2f25fffd610c1e7fbecee0277
Karen Kong <kkong006@ucr.edu>
2018-01-19 00:06:28 -0800

    Add nav drawer to home activity

--- app/src/main/java/appricottsoftware/clarity/HomeActivity.java
+++ app/src/main/java/appricottsoftware/clarity/HomeActivity.java
@@ -1,18 +1,125 @@
 package appricottsoftware.clarity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.nv_drawer) NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        // Replace toolbar
+        setSupportActionBar(toolbar);
+
+        // Set up nav drawer option clicking
+        setUpDrawer(nvDrawer);
+
+        // Set up drawer toggling
+        drawerToggle = setUpDrawerToggle();
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync state after activity startup is complete
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass changes to drawer toggle
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Drawer toggle handles events
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    private void logout() {
+        Intent loginActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginActivityIntent);
+        finish();
+    }
+
+    private void setUpDrawer(NavigationView nView) {
+        nView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawer(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectDrawer(MenuItem item) {
+        // Make fragment
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(item.getItemId()) {
+            case R.id.nav_channel_fragment:
+                break;
+            case R.id.nav_like_fragment:
+                break;
+            case R.id.nav_settings_fragment:
+                break;
+            case R.id.nav_logout:
+                logout();
+                break;
+            default:
+                break;
+        }
+
+        // Make a new fragment instance
+
+        // Insert fragment into frame layout
+
+        // Highlight the choice
+        item.setChecked(true);
+        // Set toolbar title
+        setTitle(item.getTitle());
+        // Close nav drawer
+        drawerLayout.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setUpDrawerToggle() {
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
     }
 }
