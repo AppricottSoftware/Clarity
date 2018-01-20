ff7adb020b12028ed0a7db14ee7ac9d70c1e3c8d
Karen Kong <kkong006@ucr.edu>
2018-01-19 22:53:21 -0800

    Define Channel, Like, Setting, Player Fragments

---
+++ app/src/main/java/appricottsoftware/clarity/fragments/LikeFragment.java
@@ -0,0 +1,33 @@
+package appricottsoftware.clarity.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import appricottsoftware.clarity.R;
+import butterknife.ButterKnife;
+
+/**
+ * Created by karen on 1/19/18.
+ */
+
+public class LikeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_like, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Initialize view lookups, listeners
+    }
+
+
+}
