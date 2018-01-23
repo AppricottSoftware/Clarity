39a67417f4db3c0da7b35267f548f4a4b8c2a68a
ttony678 <ttony678@yahoo.com>
2018-01-23 15:24:11 -0800

    Add Facebook button functions. Need to finish login tokens.

--- app/src/main/java/appricottsoftware/clarity/LoginActivity.java
+++ app/src/main/java/appricottsoftware/clarity/LoginActivity.java
@@ -4,11 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,11 +29,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @BindView(R.id.et_email) EditText etEmail;
     @BindView(R.id.et_password) EditText etPassword;
 
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        // Facebook Info
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
 
         btLogin.setOnClickListener(this);
         btRegister.setOnClickListener(this);
@@ -32,9 +67,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch(v.getId()) {
             case R.id.bt_login:
                 String strEmail = etEmail.getText().toString();
-                String strPassword = etPassword.getText().toString();
+                String strPassword = Hash_Password(etPassword.getText().toString());
 
-                if (isAuthenticated()) {
+                if (isAuthenticated(strEmail, strPassword)) {
                     Intent homeActivityIntent = new Intent(this, HomeActivity.class);
                     startActivity(homeActivityIntent);
                 }
@@ -51,8 +86,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public boolean isAuthenticated() {
+    public boolean isAuthenticated(String email, String password) {
         return true;
+    }
+
+    private String Hash_Password(String originalPassword) {
+        String hashedPassword = null;
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = originalPassword.getBytes("UTF-8");
+            digest.update(bytes, 0, bytes.length);
+            bytes = digest.digest();
+
+            hashedPassword = bytesToHex( bytes );
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return hashedPassword;
+    }
+
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex( byte[] bytes )
+    {
+        char[] hexChars = new char[ bytes.length * 2 ];
+        for( int j = 0; j < bytes.length; j++ )
+        {
+            int v = bytes[ j ] & 0xFF;
+            hexChars[ j * 2 ] = hexArray[ v >>> 4 ];
+            hexChars[ j * 2 + 1 ] = hexArray[ v & 0x0F ];
+        }
+        return new String( hexChars );
     }
 
 }
