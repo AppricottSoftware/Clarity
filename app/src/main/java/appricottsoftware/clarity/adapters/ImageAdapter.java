77e28b177507c4f5635c25f740dcd1f1bc9b0c82
ttony678 <ttony678@yahoo.com>
2018-01-31 17:59:41 -0800

    Add gridlayout with sample images & text within cardview

--- app/src/main/java/appricottsoftware/clarity/adapters/ImageAdapter.java
+++ app/src/main/java/appricottsoftware/clarity/adapters/ImageAdapter.java
@@ -1,13 +1,22 @@
 package appricottsoftware.clarity.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import appricottsoftware.clarity.R;
+import appricottsoftware.clarity.models.Podcast;
 
 /**
  * Created by ttony on 1/30/2018.
@@ -15,17 +24,19 @@ import appricottsoftware.clarity.R;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private ArrayList<Podcast> podcasts;
 
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c, ArrayList<Podcast> p) {
         mContext = c;
+        podcasts = p;
     }
 
     public int getCount() {
-        return mThumbIds.length;
+        return podcasts.size();
     }
 
     public Object getItem(int position) {
-        return null;
+        return podcasts.get(position);
     }
 
     public long getItemId(int position) {
@@ -34,26 +45,50 @@ public class ImageAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
+
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(450, 450));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.item_browse_result, parent, false);
+            holder.textView = convertView.findViewById(R.id.ivBrowseEpisodeName);
+            holder.imageView = convertView.findViewById(R.id.ivBrowseImg);
+            holder.imageView.setLayoutParams(new LinearLayout.LayoutParams(450, 450));
+            holder.imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            holder.imageView.setPadding(8, 8, 8, 8);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        holder.imageView.setImageResource(podcasts.get(position).getItunes_id());
+        holder.textView.setText(podcasts.get(position).getTitle_original());
+
+//        ImageView imageView;
+//        if (convertView == null) {
+//            // if it's not recycled, initialize some attributes
+//            imageView = new ImageView(mContext);
+//            imageView.setLayoutParams(new GridView.LayoutParams(450, 450));
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setPadding(8, 8, 8, 8);
+//        } else {
+//            imageView = (ImageView) convertView;
+//        }
+//
+//        imageView.setImageResource(mThumbIds[position]);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        TextView textView;
     }
 
     // references to our images
     private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
             R.drawable.sample_0, R.drawable.sample_1,
             R.drawable.sample_2, R.drawable.sample_3,
             R.drawable.sample_4, R.drawable.sample_5,
--- app/src/main/java/appricottsoftware/clarity/fragments/BrowseFragment.java
+++ app/src/main/java/appricottsoftware/clarity/fragments/BrowseFragment.java
@@ -10,12 +10,37 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import appricottsoftware.clarity.HomeActivity;
 import appricottsoftware.clarity.R;
 import appricottsoftware.clarity.adapters.ImageAdapter;
+import appricottsoftware.clarity.models.Podcast;
 import butterknife.ButterKnife;
 
 public class BrowseFragment extends Fragment {
+
+    private Integer[] images = {
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    private String[] names = {
+            "Henry", "James",
+            "Tony", "Mathew",
+            "Karen", "Gus",
+            "Patrick", "Jake",
+            "Gabriel", "Andrew",
+            "Joseph", "Charmaine",
+            "Joe", "Xhoni",
+            "Aleina", "Raymond"
+    };
 
     @Nullable
     @Override
@@ -28,8 +53,9 @@ public class BrowseFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         // Initialize view lookups, listeners
+
         GridView gridview = getActivity().findViewById(R.id.gv_browse);
-        gridview.setAdapter(new ImageAdapter(getActivity()));
+        gridview.setAdapter(new ImageAdapter(getActivity(), populatePodcasts()));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
@@ -38,5 +64,21 @@ public class BrowseFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private ArrayList<Podcast> populatePodcasts() {
+        ArrayList<Podcast> podcasts = new ArrayList<Podcast>();
+
+        for (int i = 0; i < images.length; i++) {
+            Podcast p = new Podcast();
+
+            // Using setItunes_Id temporary as the images attribute since my images
+            // are int's, not strings.
+            p.setItunes_id(images[i]);
+            p.setTitle_original(names[i]);
+            podcasts.add(p);
+        }
+
+        return podcasts;
     }
 }
