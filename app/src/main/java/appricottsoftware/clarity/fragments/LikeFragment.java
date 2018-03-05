254f660871d194300835b5a2a091d71ff76d5df6
lepatrick714 <lepatrick714@gmail.com>
2018-03-05 10:04:48 -0800

    adding new features to LikeFragment

--- app/src/main/java/appricottsoftware/clarity/fragments/LikeFragment.java
+++ app/src/main/java/appricottsoftware/clarity/fragments/LikeFragment.java
@@ -3,16 +3,21 @@ package appricottsoftware.clarity.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import appricottsoftware.clarity.R;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LikeFragment extends Fragment {
 
     private static final String TAG = "LikeFragment";
+
+    @BindView(R.id.likesRecyclerView) RecyclerView likesRecyclerView;
 
     @Nullable
     @Override
@@ -26,4 +31,6 @@ public class LikeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         // Initialize view lookups, listeners
     }
+
+
 }
